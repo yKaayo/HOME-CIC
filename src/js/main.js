d0a247642774b0f3d91cff7c5a3de2db50cd672f
yKaayo <caiokaea@gmail.com>
2024-09-11 19:51:07 -0300

    Sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,69 @@
-// Toggle Button
-const btnToggler = document.querySelector('#btn-togler');
+// Side Menu
+const btnToggler = document.querySelector("#btn-togler");
+const iconToggler = document.querySelector("#btn-togler-icon");
+
+const textToShow = document.querySelectorAll('#list-menu li a p');
 
 btnToggler.addEventListener("click", () => {
-    if (btnToggler.classList.contains("bi-justify-left")) {
-        btnToggler.classList.replace("bi-justify-left", "bi-x");
-    } else {
-        btnToggler.classList.replace("bi-x", "bi-justify-left");
+  const textList = ['Home', 'Controle', 'Relatório', 'Arquivo', 'Corporação'];
+  
+  if (iconToggler.classList.contains("bi-justify-left")) {
+    iconToggler.classList.replace("bi-justify-left", "bi-x");
+
+    // Show Text
+
+    for (let i = 0; i < textList.length; i++) {
+      textToShow[i].textContent = textList[i];
+      textToShow[i].classList.add('ms-3');
     }
-})
+  } else {
+    iconToggler.classList.replace("bi-x", "bi-justify-left");
+
+    // Hide Text
+    for (let i = 0; i < textList.length; i++) {
+      if (textToShow[i]) {
+        textToShow[i].textContent = '';
+        textToShow[i].classList.remove('ms-3');
+      }
+    }
+  }
+});
 
 // Show or Hide Password
 const eye = document.querySelector("#eye");
 const inputPassword = document.querySelector("#password");
 
 eye.addEventListener("click", () => {
-    if (inputPassword.type === "password") {
-        inputPassword.setAttribute("type", "text");
-        eye.classList.replace("bi-eye", "bi-eye-slash");
-    } else {
-        inputPassword.setAttribute("type", "password");
-        eye.classList.replace("bi-eye-slash", "bi-eye");
-    }
-})
+  if (inputPassword.type === "password") {
+    inputPassword.setAttribute("type", "text");
+    eye.classList.replace("bi-eye", "bi-eye-slash");
+  } else {
+    inputPassword.setAttribute("type", "password");
+    eye.classList.replace("bi-eye-slash", "bi-eye");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+// const btnOpenSideMenu = document.querySelector('[data-bs-target="#list"]');
+// const fieldsToCreateParagraph = document.querySelectorAll(
+//   ".navbar-nav .list-group-item a"
+// );
+// const listToCreateParagraphContent = ['Home', 'Controle', 'Relatório', 'Arquivo', 'Corporação']
+
+// btnOpenSideMenu.addEventListener("click", () => {
+//   fieldsToCreateParagraph.forEach(() => {
+//     const paragraph = document.createElement("p");
+//     paragrafo.textContent = listToCreateParagraphContent;
+//     paragrafo.classList.add();
+//     fieldsToCreateParagraph.appendChild(paragraph);
+//   });
+// });
