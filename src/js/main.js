3df860281424326e8b326daafb33707f0cda094c
yKaayo <caiokaea@gmail.com>
2024-09-16 17:12:58 -0300

    99% ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// Tooltips Bootstrap
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
